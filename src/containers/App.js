34dc8555fd8402399d2735e369e7815d528d6538
Juliane Nagao <julianens@gmail.com>
2020-02-21 14:47:15 +0100

    feat: get albums from api

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import getAlbumsAction from '../actions/albums';
 
-const App = () => <div>hello world</div>;
+class App extends React.Component {
+  componentDidMount() {
+    const { getAlbums } = this.props;
+    getAlbums();
+  }
 
-export default hot(App);
+  render() {
+    return <div>hello world</div>;
+  }
+}
+
+App.propTypes = {
+  getAlbums: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  albums: state.albums,
+});
+
+const mapDispatchToProps = {
+  getAlbums: getAlbumsAction,
+};
+
+export default hot(connect(mapStateToProps, mapDispatchToProps)(App));
