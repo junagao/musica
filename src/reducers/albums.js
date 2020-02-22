9aaccda87a739cddca7b1782df667558de6e64ba
Juliane Nagao <julianens@gmail.com>
2020-02-22 11:37:22 +0100

    feat: add albums reducer

---
+++ src/reducers/albums.js
@@ -0,0 +1,36 @@
+import {
+  GET_ALBUMS_REQUEST,
+  GET_ALBUMS_SUCCESS,
+  GET_ALBUMS_ERROR,
+} from '../actions/types';
+
+const initialState = {
+  data: [],
+  loading: false,
+  error: '',
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALBUMS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: '',
+      };
+    case GET_ALBUMS_SUCCESS:
+      return {
+        ...state,
+        data: action.albums,
+        loading: false,
+      };
+    case GET_ALBUMS_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+};
