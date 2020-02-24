c1ade4aef43f8d2392964b45cbbc62133243ea0c
Juliane Nagao <julianens@gmail.com>
2020-02-24 20:05:17 +0100

    fix: error message scss

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './ErrorMessage.scss';
+
 const ErrorMessage = ({ error }) => (
   <div className="error-message">
     <p>The following unexpected error ocurred: {error}</p>
