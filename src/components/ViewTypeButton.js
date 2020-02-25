122726f55a734b167992fab1ff47429b335d9760
Juliane Nagao <julianens@gmail.com>
2020-02-25 12:17:50 +0100

    refactor: AlbumsList and ViewTypeButton styles

--- src/components/ViewTypeButton.js
+++ src/components/ViewTypeButton.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './ViewTypeButton.scss';
+
 const ViewTypeButton = ({ onChangeView, viewType, icon }) => (
   <button
     type="button"
