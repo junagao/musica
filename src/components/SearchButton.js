8ccde28507a6ef634c859d6bfc76bd31eb3ae243
Juliane Nagao <julianens@gmail.com>
2020-02-25 12:43:58 +0100

    refactor: SearchButton and SearchContainer

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { IoIosSearch } from 'react-icons/io';
 
-import Loading from './Loading';
 import './SearchButton.scss';
 
-const SearchButton = ({ loading }) => {
-  return loading ? (
-    <Loading />
-  ) : (
-    <button type="submit" className="search-button">
-      <IoIosSearch />
-    </button>
-  );
-};
-
-SearchButton.propTypes = {
-  loading: PropTypes.bool.isRequired,
-};
+const SearchButton = () => (
+  <button type="submit" className="search-button">
+    <IoIosSearch />
+  </button>
+);
 
 export default SearchButton;
