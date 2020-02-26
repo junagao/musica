ccd5a4045502a3eacda1b7d4c62d285a08fd7b67
Juliane Nagao <julianens@gmail.com>
2020-02-26 11:38:59 +0100

    fix: index App NoAlbum

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import App from './containers/App';
+import { connectedApp as App } from './containers/App';
 import store from './store';
 
 import './index.scss';
