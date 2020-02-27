950db48bd98bf5c26c9b709677190dc18a63a87a
Juliane Nagao <julianens@gmail.com>
2020-02-27 17:28:09 +0100

    feat: update api

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,10 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://itunes.apple.com/search',
-  headers: { 'Access-Control-Allow-Origin': '*' },
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+  },
 });
