36a7522403ef329fc8b6e8141030c97655541b4d
Juliane Nagao <julianens@gmail.com>
2020-03-01 21:26:15 +0100

    feat: add previous next page navigation

--- src/reducers/pagination.js
+++ src/reducers/pagination.js
@@ -2,12 +2,14 @@ import { CHANGE_PAGE_NUMBER } from '../actions/types';
 
 const initialState = {
   currentPage: 1,
+  albumsPerPage: 20,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_PAGE_NUMBER:
       return {
+        ...state,
         currentPage: action.pageNumber,
       };
     default:
