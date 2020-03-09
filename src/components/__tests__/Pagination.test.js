4c7a484c23669429fa44d9ffc77aa8d4dfb7907a
Juliane Nagao <julianens@gmail.com>
2020-03-09 14:59:12 +0100

    feat: add PaginationItem tests

--- src/components/__tests__/Pagination.test.js
+++ src/components/__tests__/Pagination.test.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Pagination from 'components/Pagination';
+import PaginationItem from 'components/PaginationItem';
 
 describe('Pagination', () => {
   let wrapper;
-  const mockAlbummsPerPage = 5;
-  const mockTotalAlbums = 40;
+  const currentPageMock = 1;
+  const albummsPerPageMock = 5;
+  const totalAlbumsMock = 40;
   const onChangePageMock = jest.fn();
 
   beforeEach(() => {
     wrapper = shallow(
       <Pagination
-        albumsPerPage={mockAlbummsPerPage}
-        totalAlbums={mockTotalAlbums}
+        currentPage={currentPageMock}
+        albumsPerPage={albummsPerPageMock}
+        totalAlbums={totalAlbumsMock}
         onChangePage={onChangePageMock}
       />,
     );
@@ -35,63 +38,11 @@ describe('Pagination', () => {
     expect(wrapper.find('ul').length).toEqual(1);
   });
 
-  it('should render 8 li element', () => {
-    expect(wrapper.find('li').length).toEqual(8);
+  it('should render 10 instances of PaginationItem element', () => {
+    expect(wrapper.find(PaginationItem).length).toEqual(10);
   });
 
   it('should render 8 li element with className `page-item`', () => {
     expect(wrapper.find('.page-item').every('.page-item')).toBeTruthy();
-  });
-
-  it('should render all `a` elements with corresponded href prop and className `page-link`', () => {
-    expect(wrapper.find('a').length).toEqual(8);
-    expect(
-      wrapper
-        .find('a')
-        .at(0)
-        .prop('href'),
-    ).toEqual('#1');
-    expect(
-      wrapper
-        .find('a')
-        .at(0)
-        .hasClass('page-link'),
-    ).toBeTruthy();
-    expect(
-      wrapper
-        .find('a')
-        .at(1)
-        .prop('href'),
-    ).toEqual('#2');
-    expect(
-      wrapper
-        .find('a')
-        .at(1)
-        .hasClass('page-link'),
-    ).toBeTruthy();
-  });
-
-  it('should render the pages numbers', () => {
-    expect(
-      wrapper
-        .find('a')
-        .at(0)
-        .text(),
-    ).toEqual('1');
-    expect(
-      wrapper
-        .find('a')
-        .at(1)
-        .text(),
-    ).toEqual('2');
-  });
-
-  it('should call onChangePage prop passing the page number', () => {
-    const number = 4;
-    wrapper
-      .find('a')
-      .at(3)
-      .simulate('click', number);
-    expect(onChangePageMock).toBeCalledWith(4);
   });
 });
